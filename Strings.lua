59979273d1974d734dbbed33cda0f73ca4a79e25
aguywhathe <aguywhathe@gmail.com>
2026-05-09 22:22:19 -0400

    Update Strings.lua

--- Strings.lua
+++ Strings.lua
@@ -1,17 +1,7 @@
 -- Collection of strings later formatted by the decompiler for output
 local MemeStrings = {
-	" boo 👻",
-	" Exceeded decompiler timeout.",
-	" DECOMPILED BY ADVANCED DECOMPILER V3",
-	" DISASSEMBLED...",
-	" Decompiled with the Synapse X Luau decompiler.",
-	" 𝓓𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓭 𝓫𝔂 𝓯𝓻𝓮𝓪𝓴𝔂 𝓭𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓻",
-	"SynapseX Decompiler",
-	"decompiler is slow, removed right now :(",
-	"NOTE: Currently in beta! Not representative of final product.",
-	" params : ...",
-	" " .. os.date(),
-	" your advertisement could be here"
+	" DISASSEMBLED BY ADVANCED DECOMPILER V3",
+	" DISASSEMBLED OUTPUT...",
 }
 
 local Strings = {
